6eb81978d77dc3a36a76105497ed5311348907ab
iago ferro <134851855+yagoFerro@users.noreply.github.com>
2023-05-30 20:04:36 -0300

    aula

---
+++ ex024/app.js
@@ -0,0 +1,14 @@
+const link = document.querySelector('a')
+
+//console.log(link.getAttribute('href'))
+
+link.setAttribute('href', 'https://github.com/yagoFerro')
+link.innerHTML ='WebSite do iago Ferro'
+const paragraph = document.querySelector('p')
+
+console.log(paragraph.getAttribute('class'))
+
+
+paragraph.setAttribute('class', 'success')
+
+paragraph.setAttribute('style', 'color: red;')
