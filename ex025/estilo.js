94c8b83f32551fb5908ac967b736d6cfd031aa91
iago ferro <134851855+yagoFerro@users.noreply.github.com>
2023-05-31 14:39:28 -0300

    revision

---
+++ ex025/estilo.js
@@ -0,0 +1,46 @@
+const title = document.querySelector('h1')
+
+title.classList.toggle('test')
+
+title.classList.toggle('title')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const paragraphs =document.querySelectorAll('p')
+//
+//paragraphs.forEach(paragraph => {
+//    if(paragraph.textContent.includes('error')) {
+//        paragraph.classList.add('error')
+//    }
+//    if(paragraph.textContent.includes('success')){
+//        paragraph.classList.add('success')
+//    }
+//
+//})
+//
+
+//const paragraph = document.querySelector('p')
+//console.log(paragraph.classList)
+//
+//paragraph.classList.add('error')
+//
+//paragraph.classList.remove('error')
+//
+//paragraph.classList.add('success')
+//
