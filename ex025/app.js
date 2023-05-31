2d04d25700510e2f7d8d62d238b33c6e220b2034
iago ferro <134851855+yagoFerro@users.noreply.github.com>
2023-05-31 14:01:52 -0300

    estudos

---
+++ ex025/app.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('h1')
+
+//title.setAttribute('style', 'margin: 50px;')
+
+console.log(title.style)
+console.log(title.style.color)
+
+title.style.backgroundColor = 'red'
+title.style.color = 'green'
+
+title.style.fontSize = '3em'
+
+title.style.backgroundColor= ''
